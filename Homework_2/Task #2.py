ab5d8bcb5632245d43f26f69a41e3d797926faef
Your Name <you@example.com>
2025-11-03 12:42:48 +0400

    davaleba #2

---
+++ Homework_2/Task #2.py
@@ -0,0 +1,10 @@
+subject={
+    'math':{'George':85, 'Salome':78, 'David':92},
+    'Physics':{'George':90, 'David':75, 'Salome':88},
+    'Chemistry':{'David':82, 'George':80, 'Salome':91},
+}
+Output={'George': {'math': 85, 'physics': 90, 'chemistry': 80},
+        'Salome': {'math': 78, 'physics': 88, 'chemistry': 91},
+        'David': {'math': 92, 'physics': 75, 'chemistry': 82}
+        }
+print(Output)
