ce8d165f05e8c2180ee30a6888a514d1ede1e86d
Your Name <you@example.com>
2025-12-14 13:53:20 +0400

    Homework #4_Task1_ჩასწორებული

--- Homework_4/Task_1.py
+++ Homework_4/Task_1.py
@@ -7,12 +7,12 @@ def typeofnumber(n):
         return True
     if n%2==0:
         return False
-    i=3
-    while i < n:
-        if n%i==0:
-            i+=2
+    if n>=3:
+        while i < n:
+           if n%i==0:
             return False
     return True
-rincxvebi=[1,0,27,12,15,-12]
+
+rincxvebi=[1,5,12,22,21,75]
 for i in rincxvebi:
     print(i, typeofnumber(i))
