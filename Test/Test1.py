d6242e5af2ac177a3a288cad2c97780b7b73abba
Your Name <you@example.com>
2026-01-10 13:02:16 +0400

    კარტების თამაში

--- Test/Test1.py
+++ Test/Test1.py
@@ -1,13 +1,1 @@
-import csv
-filename="user2.csv"
-users=[
-    {"name":"aladina","age":32},
-    {"name":"gia","age":69},
-    {"name":"nino","age":58}
-]
-user={"name":"aladina","age":32}
-with open(filename,'w',newline='') as file:
-    column=['name','age']
-    writer=csv.DictWriter(file,fieldnames=column)
-    writer.writeheader()
-    writer.writerow(users)
+retururn a*b//gcd(a,b)
